139931a3ae780bdbe2f83e8ce11b1cd1cc2e8b1f
Sagar Rathod <97072541+sagarbrathod1@users.noreply.github.com>
2023-10-05 02:40:56 -0400

    added initial about me buttons and section divider

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import React from "react";
-import Image from "next/image";
 import headshotImg from "@/public/headshot.png";
 import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
@@ -42,6 +44,44 @@ export default function Intro() {
         Scientist (SCAs & HIV-1). <br />↣ Michigan State Grad.{" "}
         <span className="green-text">Go Green!</span>
       </motion.h1>
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        {/* <Link
+          href="#contact"
+          className="group bg-slate-300 px-7 py-3 flex items-center gap-2 rounded-full"
+        >
+          Contact Me
+        </Link> */}
+        <a
+          href="https://github.com/sagarbrathod1/resume/blob/main/Resume%20-%20Sagar%20Rathod.pdf/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-red-600 px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-green-500 active:scale-105 transition borderBlack"
+        >
+          Resume
+        </a>
+        <a
+          className="bg-slate-300 p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition borderBlack"
+          href="https://www.linkedin.com/in/sagarbrathod1/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-slate-300 p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition borderBlack text-[1.35rem] hover:text-gray-950 "
+          href="https://github.com/sagarbrathod1"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
