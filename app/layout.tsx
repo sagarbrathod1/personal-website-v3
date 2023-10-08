8740ea89057eeaa8a212a1696df1b46e3acebb17
Sagar Rathod <97072541+sagarbrathod1@users.noreply.github.com>
2023-10-08 01:46:19 -0400

    let's see if rename works for vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import Footer from "@/components/footer";
-import Header from "@/components/Header";
+import Header from "@/components/header";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
