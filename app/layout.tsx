2435b4847f49b22e486d87bba54bb36472ac61a2
Sagar Rathod <97072541+sagarbrathod1@users.noreply.github.com>
2023-10-08 01:44:13 -0400

    rename error with vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import Footer from "@/components/footer";
-import Header from "@/components/header";
+import Header from "@/components/Header";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
