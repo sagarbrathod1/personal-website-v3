4945dbb3d9b9e88528f468c72d4603984a1fe7f2
Sagar Rathod <97072541+sagarbrathod1@users.noreply.github.com>
2023-10-07 22:50:25 -0400

    fix spacing and import

--- components/skills.tsx
+++ components/skills.tsx
@@ -21,7 +21,7 @@ const fadeInAnimationVariants = {
 
 export default function Skills() {
   return (
-    <section className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40">
+    <section className="mb-20 max-w-[53rem] text-center">
       <SectionHeading>My Skills</SectionHeading>
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-white">
         {skillsData.map((skill, index) => (
